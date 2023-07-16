b9657291ff37208217727538bde10ba4f6abd666
tengjiaoji <296840095@qq.com>
2023-07-16 13:21:27 +0800

    v1.0

--- 工程化学习部分/构造工具的学习/webpack-proj/src/main.js
+++ 工程化学习部分/构造工具的学习/webpack-proj/src/main.js
@@ -1,12 +1,23 @@
-import z,{b} from './a';
-import $ from 'jquery';
-import x,{a} from './b';
-import url from './20230707094805.jpg'
-import './1.less'
-import './2.less' 
-import home from './home'
-import getImg from './move/a/b'
-getImg();
-console.log(a,b,home)
-$('<h1>').text('Hello,jquery!!!').css({ "color": "#ff0011", "background": "blue" }).appendTo(document.body)
-$('<img>').prop('src',url).appendTo(document.body)
+// import z,{b} from './a';
+// import $ from 'jquery';
+// import x,{a} from './b';
+// import url from './20230707094805.jpg'
+// import './1.less'
+// import './2.less' 
+// import home from './home'
+// import getImg from './move/a/b'
+
+import styles1 from './a.module.less'
+import styles2 from './b.module.less'
+
+import $ from 'jquery'
+console.log(styles1,styles2)
+
+$('<h1>').text('hello world').addClass(styles1 .a).appendTo('body')
+$('<h1>').text('hello world!!!').addClass(styles2 .a).appendTo('body')
+
+// getImg();
+// console.log(a,b,home)
+// $('<h1>').text('Hello,jquery!!!').css({ "color": "#ff0011", "background": "blue" }).appendTo(document.body)
+// $('<img>').prop('src',url).appendTo(document.body)
+
