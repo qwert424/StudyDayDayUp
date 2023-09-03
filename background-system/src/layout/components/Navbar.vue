41c9d948303c8a4b7a069a80d178dfe0ed575428
tengjiaoji <296840095@qq.com>
2023-09-03 18:58:16 +0800

    v1.0

--- background-system/src/layout/components/Navbar.vue
+++ background-system/src/layout/components/Navbar.vue
@@ -14,24 +14,12 @@
           <img :src="avatar + '?imageView2/1/w/80/h/80'" class="user-avatar" />
           <i class="el-icon-caret-bottom" />
         </div>
-        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+        <el-dropdown-menu slot="dropdown" class="user-dropdown hader-new-drop">
           <router-link to="/">
-            <el-dropdown-item> Home </el-dropdown-item>
+            <el-dropdown-item> 个人中心 </el-dropdown-item>
           </router-link>
-          <a
-            target="_blank"
-            href="https://github.com/PanJiaChen/vue-admin-template/"
-          >
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a
-            target="_blank"
-            href="https://panjiachen.github.io/vue-element-admin-site/#/"
-          >
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display: block">Log Out</span>
+            <span style="display: block">退出登录</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -64,8 +52,11 @@ export default {
   },
 };
 </script>
-
 <style lang="scss" scoped>
+.hader-new-drop {
+  top: 50px !important;
+  left: 1260px !important;
+}
 .navbar {
   height: 50px;
   overflow: hidden;
@@ -92,6 +83,7 @@ export default {
 
   .right-menu {
     float: right;
+    margin-right: 12px;
     height: 100%;
     line-height: 50px;
 
